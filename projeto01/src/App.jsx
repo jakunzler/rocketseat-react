b4258c3d08206870b02d095364134643433ea1c2
Jonas Kunzler <k.jonasaugusto@gmail.com>
2024-06-03 11:31:28 -0300

    feat: first steps ReactJS

---
+++ projeto01/src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <div>
+      <h1>Meu primeiro projeto React</h1>
+      <p>Estou gostando de aprender React</p>
+      <Post />
+    </div>
+  )
+}
